4c55310f9064f63bd81ed121e0bfc2bed0a3071e
Zenith1997 <dabarerawza.19@uom.lk>
2024-12-17 11:29:52 +0530

    first commit

---
+++ Q1/1.1.js
@@ -0,0 +1,21 @@
+Date.prototype.daysTo = function (date) {
+  if (!(date2 instanceof Date)) {
+    throw new Error("The argument must be a valid Date object");
+  }
+
+  // Calculating the time difference
+  // of two dates
+  let Difference_In_Time = date.getTime() - this.getTime();
+
+  // Calculating the no. of days between
+  // two dates
+  let Difference_In_Days = Math.floor(Difference_In_Time / (1000 * 3600 * 24));
+  return Difference_In_Days < 0
+    ? Math.abs(Difference_In_Days)
+    : Difference_In_Days;
+};
+
+let date2 = new Date("01/1/2024 00:00:00");
+let date1 = new Date("01/2/2024 12:00:00");
+
+console.log(date1.daysTo(date2)); // Correctly calls the method and outputs: 10
