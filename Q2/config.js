247732082cf07d10645120bdf224ac894bb0561b
Dabarera <75031785+Zenith1997@users.noreply.github.com>
2024-12-17 23:42:10 +0530

    Add files via upload

---
+++ Q2/config.js
@@ -0,0 +1,7 @@
+// config.js
+module.exports = {
+  CLIENT_ID: "",
+  CLIENT_SECRET: "",
+  REDIRECT_URI: "", // Example: http://localhost:3000/oauth2callback
+  REFRESH_TOKEN: "",
+};
---
+++ Q2/index.js
@@ -0,0 +1,15 @@
+// server.js
+const express = require("express");
+const routes = require("./routes");
+
+const app = express();
+app.use(express.json());
+
+// Register routes
+app.use("/", routes);
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
